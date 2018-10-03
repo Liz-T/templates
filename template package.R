a009b90fb7844234dcc19a8f2bea24392cb1f852
Liz <>
2018-10-03 21:00:27 +0100

    changed package location

---
+++ template package.R
@@ -0,0 +1,14 @@
+# create package for markdown template
+
+devtools::create("analysisTemplate")
+dir.create("analysisTemplate/inst/rmarkdown/templates/documentation/skeleton",
+           recursive = TRUE)
+
+# save a markdown template into the skeleton folder
+# save a text file with the below format into the skeleton folder, the blank line is important.
+# name: blarblar
+#
+
+devtools::install("analysisTemplate")
+library(analysisTemplate)
+remove.packages("analysisTemplate")
